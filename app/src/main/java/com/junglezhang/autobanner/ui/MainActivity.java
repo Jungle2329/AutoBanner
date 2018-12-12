f511df64cabc326614b148073aab403e5b28df9f
HeroJungle <zhangyi_jungle@sina.com>
2018-12-12 10:23:15 +0800

    sample image change

--- app/src/main/java/com/junglezhang/autobanner/ui/MainActivity.java
+++ app/src/main/java/com/junglezhang/autobanner/ui/MainActivity.java
@@ -32,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
         AutoBannerIndicator ab_banner_indicator = findViewById(R.id.ab_banner_indicator);
 
 
-        String banner1 = "https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=3609026444,888457979&fm=173&app=25&f=JPEG?w=580&h=326&s=72B11DC7406A7F114B3E18BA03001018";
-        String banner2 = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4109200045,2976713416&fm=11&gp=0.jpg";
-        String banner3 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544444887665&di=3f2f7bf615b814eb9f3a55c57a107a09&imgtype=0&src=http%3A%2F%2F03imgmini.eastday.com%2Fmobile%2F20181108%2F20181108210857_0a78b3f6c6179087749434b4fc92ad82_1.jpeg";
-        String banner4 = "https://raw.githubusercontent.com/JessYanCoding/ArmsComponent/master/arts/arms_component_banner_v1.0.jpg";
+        String banner1 = "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3609500805,1555342859&fm=26&gp=0.jpg";
+        String banner2 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544591288175&di=d82793880e749f5c8992427887d26f5e&imgtype=0&src=http%3A%2F%2Fjtgeek.com%2Fwp-content%2Fuploads%2Fandroid-logo.jpg";
+        String banner3 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544591288175&di=f0a6a178e864815d98ada16756a4565f&imgtype=0&src=http%3A%2F%2Fwww.arinchina.com%2Fupload%2Fportal%2F201705%2F18%2F173937rzzhno9imm8iy8bw.jpg";
+        String banner4 = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544591288171&di=9af13bb975846efe92a43e8e68bf40d1&imgtype=0&src=http%3A%2F%2Fn1.itc.cn%2Fimg8%2Fwb%2Frecom%2F2016%2F04%2F22%2F146126165500061409.PNG";
         String[] banners = new String[]{banner1, banner2, banner3, banner4};
         List<BannerData> list = new ArrayList<>();
         for (String s : banners) {
